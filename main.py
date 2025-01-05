eef137bcceca85ad0c59f1f8c9ac1460df85d410
RealDataDojo <datadojocaptain@gmail.com>
2025-01-05 23:08:06 +0800

    remove get transcription

--- main.py
+++ main.py
@@ -9,37 +9,26 @@ import streamlit as st
 load_dotenv()
 openai_api_key = os.getenv('OPENAI_API_KEY')
 llm_model = 'gpt-4o-mini'
-system_prompt = 'You are a youtube summarizer. Create a short, informative summary of the main points of the transcript text here: ' 
+system_prompt = 'You are a tweet writer. Create a viral tweet based on the text: ' 
 
 
-def extract_transcript(url):
-    res = ''
-    video_id = re.split(r'&|=', url)[1]
-    transcripts = YouTubeTranscriptApi.get_transcript(video_id)
-
-    for transcript in transcripts:
-        res += ' ' + transcript['text']
-    return res
-
-
-def generate_summary(res):
+def generate_summary(input_text):
     client = OpenAI(api_key=openai_api_key)
 
     chat_completion = client.chat.completions.create(
         model=llm_model,
-        messages=[{"role": "system", "content": system_prompt}] + [{"role": "user", "content": res}]
+        messages=[{"role": "system", "content": system_prompt}] + [{"role": "user", "content": input_text}]
     )
     return chat_completion.choices[0].message.content
 
 
 if __name__ == "__main__":
-    st.title("Youtube Summarizer")
-    st.subheader("Get notes from videos ASAP")
-    input_url = st.text_input("Enter your url")
-    button = st.button("Generate video summary")
+    st.title("Viral Tweets")
+    st.subheader("Make your blog post into viral tweets instantly")
+    input_text = st.text_input("Enter text")
+    button = st.button("Get viral tweets")
     if button:
-        with st.spinner("Generating summary... Please wait"):
-            res = extract_transcript(input_url)
-            summary = generate_summary(res)
-        st.text(summary)
+        with st.spinner("Generating tweets... Please wait"):
+            res = generate_summary(input_text)
+        st.text(res)
 
